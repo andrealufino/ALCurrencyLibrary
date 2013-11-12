33003404ad345d3255fb68516984b8b6e16d9c36
Andrea Mario Lufino <andrea.lufino@me.com>
2013-11-12 11:06:27 +0100

    Added ALCurrency class

---
+++ ALCurrencyLibrary/ALCurrencyLibrary/ALCurrency.h
@@ -0,0 +1,13 @@
+//
+//  ALCurrency.h
+//  ALCurrencyLibrary
+//
+//  Created by Andrea Mario Lufino on 12/11/13.
+//  Copyright (c) 2013 Andrea Mario Lufino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALCurrency : NSObject
+
+@end
---
+++ ALCurrencyLibrary/ALCurrencyLibrary/ALCurrency.m
@@ -0,0 +1,13 @@
+//
+//  ALCurrency.m
+//  ALCurrencyLibrary
+//
+//  Created by Andrea Mario Lufino on 12/11/13.
+//  Copyright (c) 2013 Andrea Mario Lufino. All rights reserved.
+//
+
+#import "ALCurrency.h"
+
+@implementation ALCurrency
+
+@end
