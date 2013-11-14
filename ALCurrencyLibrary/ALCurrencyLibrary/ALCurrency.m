f9cb15f3c836b6fdf0a8ee0e6c33c4515001c214
Andrea Mario Lufino <andrea.lufino@me.com>
2013-11-14 15:29:07 +0100

    Documented the .m file of ALCurrency

--- ALCurrencyLibrary/ALCurrencyLibrary/ALCurrency.m
+++ ALCurrencyLibrary/ALCurrencyLibrary/ALCurrency.m
@@ -24,9 +24,12 @@
 + (void)convertFromCurrency:(NSString *)fromCurrency toCurrency:(NSString *)toCurrency value:(CGFloat)value
                completionBlock:(ALCurrencyCompletionBlock)onCompletion failureBlock:(ALCurrencyFailureBlock)onFailure {
     
+    // Create the dictionary which will contains the parameters
     NSDictionary *parameters = @{@"from": fromCurrency, @"to": toCurrency, @"q": @(value)};
     
+    // Create the mutable request with GET method, "currency" url and the dictionary parameters
     NSMutableURLRequest *request = [[ALCLHTTPClient sharedHTTPClient] requestWithMethod:@"GET" path:kURL parameters:parameters];
+    // Create the JSON operation and assign the completion and failure blocks
     AFJSONRequestOperation *requestOperation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
         onCompletion(YES, [JSON[@"v"] floatValue]);
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
@@ -34,6 +37,7 @@
         onFailure(error);
     }];
     
+    // Start the request
     [[ALCLHTTPClient sharedHTTPClient] enqueueHTTPRequestOperation:requestOperation];
 }
 
